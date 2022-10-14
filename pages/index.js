b31ff45693cb3c0a71df6f305fa55b419b665139
Pedro Simao <pedrosimao@gmail.com>
2022-10-14 08:01:04 +0100

    update delay and transition values

--- pages/index.js
+++ pages/index.js
@@ -17,9 +17,9 @@ function Section ({sectionTitle, bgColor}) {
   return (
     <div className="snap-start">
       <motion.div
-        initial={{scale: .5, opacity: 0}}
+        initial={{scale: .8, opacity: 0}}
         whileInView={{scale: 1, opacity: 1}}
-        transition={{ duration: 1.1, delay: .5 }}
+        transition={{ duration: 1.5, delay: .0 }}
         viewport={{ once: false, amount: 0.5 }}
         className='section' >
         <div className={classes}>{sectionTitle}</div>
